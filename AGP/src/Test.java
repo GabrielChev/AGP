b7b77b9e4229762156a179361b5105347bfbe031
walid <walid@DESKTOP-459TSRR>
2020-01-10 11:30:34 +0100

    walid

--- AGP/src/Test.java
+++ AGP/src/Test.java
@@ -3,6 +3,6 @@ public class Test {
 	public static void main (String [] argv) 
 	{
 		System.out.println("je suis dans le main ");
-		System.out.println("Tais");
+		System.out.println("walid");
 	}
 }
