2e3c67c07a9173fe7c5c8f88a2e8f6067876ece8
walid <walid@DESKTOP-459TSRR>
2020-01-10 14:17:01 +0100

    walid comm

--- AGP/src/DAO/Test.java
+++ AGP/src/DAO/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main (String [] args) 
 	{
 		System.out.println("je suis dans le main ");
-		System.out.println("ça fait plaisir");
+		System.out.println("ça fait");
 	}
 }
