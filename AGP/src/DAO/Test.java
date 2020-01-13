a174de021efc494b15259153bf12d98976d776f6
macbookpro <macbookpro@localhost>
2020-01-13 21:23:06 +0100

    Lucene framework

--- AGP/src/DAO/Test.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-public class Test {
-	
-	public static void main (String [] args) 
-	{
-		System.out.println("je suis dans le main ");
-		System.out.println("ça fait");
-	}
-}
--- AGP/src/buisness/objects/TransportWay.java
+++ AGP/src/buisness/objects/TransportWay.java
@@ -2,34 +2,4 @@ package buisness.objects;
 
 public class TransportWay {
 
-		private String type;
-		private double kilometerPrice;
-		
-		public TransportWay(String type, double kilometerPrice)
-		{
-			this.type = type;
-			this.kilometerPrice = kilometerPrice;
-		}
-
-		public String getType() {
-			return type;
-		}
-
-		public void setType(String type) {
-			this.type = type;
-		}
-
-		public double getKilometerPrice() {
-			return kilometerPrice;
-		}
-
-		public void setKilometerPrice(double kilometerPrice) {
-			this.kilometerPrice = kilometerPrice;
-		}
-
-		@Override
-		public String toString() {
-			return "TransportWay [type=" + type + ", kilometerPrice=" + kilometerPrice + "]";
-		}
-		
 }
