7a0cde1b29e5297f195ee00c145904e2146d5fb8
Gabriel <Gabriel@10.77.2.251>
2020-01-14 17:10:47 +0100

    Mini test v2

--- AGP/src/tests/TestConnexionJdbc.java
+++ AGP/src/tests/TestConnexionJdbc.java
@@ -1,20 +1,19 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import com.mysql.jdbc.PreparedStatement;
 
 import persistance.JdbcConnection;
 
-class TestConnexionJdbc {
+public class TestConnexionJdbc {
 
 	@Test
-	void test() {
+	public void test() {
+		System.out.println("Test Select Count Hotel");
 		int idEntry = 0;
 		try {
 		
@@ -33,4 +32,41 @@ class TestConnexionJdbc {
 			System.err.println(se.getMessage());
 		}
 	}
+	@Test
+	public void testSelectMultiple() {
+		System.out.println("Test Select id and name isle");
+	//	String selectAddressQuery = "SELECT DISTINCT Isle.id_isle AS idIsle,Isle.name AS isleName FROM Isle,Hotel WHERE Isle.id_isle = Hotel.id_isle";
+		String selectAddressQuery = "SELECT Isle.id_isle AS idIsle,Isle.name AS isleName FROM Isle";
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) JdbcConnection.getConnection().prepareStatement(selectAddressQuery);
+			
+			ResultSet result = preparedStatement.executeQuery();
+			while(result.next()) {
+				System.out.println(result.getString("idIsle") + " : " + result.getString("isleName"));
+			}
+			preparedStatement.close();
+			
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+	}
+	
+	@Test
+	public void testSelectMultiple2() {
+		System.out.println("Test Select");
+		String selectAddressQuery = "SELECT * FROM TouristicSite";
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) JdbcConnection.getConnection().prepareStatement(selectAddressQuery);
+			
+			ResultSet result = preparedStatement.executeQuery();
+			while(result.next()) {
+				System.out.println(result.getString("id_touristicSite") + " : " + result.getString("name"));
+			}
+			preparedStatement.close();
+			
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+	}
+	
 }
