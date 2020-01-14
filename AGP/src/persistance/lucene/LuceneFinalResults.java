6e5c43548b6c34e7ce1521dd69eb06f0f9a2ae3d
macbookpro <macbookpro@localhost>
2020-01-15 00:51:19 +0100

    Lucene Finale result class

--- AGP/src/persistance/lucene/LuceneFinalResults.java
+++ AGP/src/persistance/lucene/LuceneFinalResults.java
@@ -13,25 +13,29 @@ import java.sql.DriverManager;
 import persistance.JdbcConnection;
 
 public class LuceneFinalResults {
-			
+		
 		private ArrayList<String> selectedFiles =  new ArrayList<String>();
 		private ArrayList<String> queryResult =  new ArrayList<String>();
-		private String query;
-		
+		private String query="";
 		public  LuceneFinalResults(String SearchingWord , String query) throws SQLException {
-			this.query = query;
-			
+			String fileSelected = "";
+
 			LuceneQuery luceneQuery = new LuceneQuery(SearchingWord);
 			 selectedFiles = luceneQuery.getPathList();
+			 
+			 
+			 fileSelected = generateString(selectedFiles); 
+			  query = query+" WHERE description IN "+fileSelected;
+		
+			
+			this.query = query;
+		
 			 queryResult = mergeResult();
+			 System.out.println("Lucene Search Found File : " + selectedFiles);
+			 System.out.println("Query Result :  "+ queryResult);
 			 
 		}
-		public ArrayList<String> getSelectedFiles() {
-			return selectedFiles;
-		}
-		public void setSelectedFiles(ArrayList<String> selectedFiles) {
-			this.selectedFiles = selectedFiles;
-		}
+
 		
 		
 		public ArrayList<String> getQueryResult() {
@@ -52,9 +56,10 @@ public class LuceneFinalResults {
 				String line = "";
 				int index = 1;
 				while (result.next()) {
-				
-					queryResult.add(result.getString(index));
-					System.out.println(result.getFloat(1));
+					line = result.getString("name") +";"+result.getString("type")+";"+result.getFloat("longitude")+
+							";"+result.getFloat("latitude")+";"+result.getInt("id_isle");
+					queryResult.add(line);
+			
 				}
 				
 				preparedStatement.close();
@@ -70,5 +75,16 @@ public class LuceneFinalResults {
 		public void setQuery(String query) {
 			this.query = query;
 		}
-	
+		public  String generateString(ArrayList<String> selectedFiles) {
+			String filesSelected = "(";
+			for (int i = 0 ; i< selectedFiles.size();i++) {
+				if(i == selectedFiles.size()-1) {
+					filesSelected += "'"+selectedFiles.get(i)+"')";
+				}else {
+					filesSelected += "'"+selectedFiles.get(i)+"',";
+			
+				}
+			}	
+			return filesSelected;
+		}
 }
