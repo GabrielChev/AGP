b9781eda321952939391d58b95cc514f4be0e326
brahim <brahim@brahim-Latitude-E6520>
2020-01-14 14:53:37 +0100

    Lucene Indexation and Searching.

---
+++ AGP/src/persistance/lucene/LuceneConstants.java
@@ -0,0 +1,10 @@
+package persistance.lucene;
+
+public class LuceneConstants {
+	public static final String CONTENTS = "contents";
+	public static final String FILE_NAME = "filename";
+	public static final String FILE_PATH = "filepath";
+	public static final int MAX_SEARCH = 10;
+	public static final String INDEX_DIR = "/home/brahim/eclipse-workspace/AGP/AGP/src/persistance/Index";
+	public static final  String DATA_DIR = "/home/brahim/eclipse-workspace/AGP/AGP/src/persistance/description";
+}
---
+++ AGP/src/persistance/lucene/LuceneTester.java
@@ -0,0 +1,84 @@
+package persistance.lucene;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import java.io.File;
+public class LuceneTester {
+	
+
+   Indexer indexer;
+   Searcher searcher;
+   
+  public ArrayList<String> LuceneTester (String searchingWord) {
+	 ArrayList<String> pathList = new ArrayList<String>();
+      try {
+      	 if (isDirectoryEmpty(new File(LuceneConstants.INDEX_DIR))) {
+         	createIndex();
+         }
+         pathList = search(searchingWord,pathList);
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (ParseException e) {
+         e.printStackTrace();
+      }
+	return pathList;
+   }
+  /* public static void main (String[] argv) {
+	   LuceneTester tester;
+	      try {
+	      tester = new LuceneTester();
+	      	 if (isDirectoryEmpty(new File(LuceneConstants.INDEX_DIR))) {
+	          	tester.createIndex();
+	          }
+	      tester.search("mosquée");
+	      
+	      System.out.println("array "+pathList);
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (ParseException e) {
+	         e.printStackTrace();
+	      }
+   }*/
+   
+   
+   private void createIndex() throws IOException {
+		      indexer = new Indexer(LuceneConstants.INDEX_DIR);
+		      int numIndexed;
+		      long startTime = System.currentTimeMillis();	
+		      numIndexed = indexer.createIndex(LuceneConstants.DATA_DIR, new TextFileFilter());
+		      long endTime = System.currentTimeMillis();
+		      indexer.close();
+		      System.out.println(numIndexed+" File indexed, time taken: "
+		         +(endTime-startTime)+" ms");
+	 
+   }	   
+   private  ArrayList<String>  search(String searchQuery, ArrayList<String> pathList) throws IOException, ParseException {
+      searcher = new Searcher(LuceneConstants.INDEX_DIR);
+      long startTime = System.currentTimeMillis();
+      TopDocs hits = searcher.search(searchQuery);
+      long endTime = System.currentTimeMillis();
+   
+      System.out.println(hits.totalHits +
+         " documents found. Time :" + (endTime - startTime));
+      for(ScoreDoc scoreDoc : hits.scoreDocs) {
+         Document doc = searcher.getDocument(scoreDoc);
+           /* System.out.println("File: "
+            + doc.get(LuceneConstants.FILE_PATH)+" score : "+scoreDoc);*/
+            pathList.add(doc.get(LuceneConstants.FILE_PATH));
+      }  
+      return  pathList;
+   }
+   
+   public static boolean isDirectoryEmpty(File file){
+	   if(file != null && file.isDirectory()){
+	    return ( file.list().length == 0 ? true : false); 
+	   }
+	   return false;
+	  }
+	 
+}
