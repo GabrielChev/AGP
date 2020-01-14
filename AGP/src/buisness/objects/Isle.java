1bda1a5d24d2d886905123ee3bf3f021c076f83b
brahim <brahim@brahim-Latitude-E6520>
2020-01-14 10:08:02 +0100

    FINAL OBJECT COMMIT.

---
+++ AGP/src/buisness/objects/Isle.java
@@ -0,0 +1,26 @@
+package buisness.objects;
+
+public class Isle {
+
+		private String name;
+		
+		public Isle(String name)
+		{
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "Isle [name=" + name + "]";
+		}
+		
+		 
+}
