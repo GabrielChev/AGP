2d19686b60adadee167e1e20060c8fb83cd1aed4
DepInfo <DepInfo@DESKTOP-UVLFVJV>
2020-01-15 10:47:06 +0100

    Finish of applying Builder Design Pattern for creating Excursion Object Adding of 1 interface (TransportOutil)+ 4 classes (Bus , Boat , Excursion,  ExcursionBuilder)

--- AGP/src/buisness/model/Excursion.java
+++ AGP/src/buisness/model/Excursion.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import buisness.objects.TouristicSite;
 
-public class Excursion {
-	private List<TransportOutil> transports;
-	private List<TouristicSite> sites;
+class Excursion {
 	
+	private List<TouristicSite> sites;
+	private List<TransportOutil> transports;
+		
+	//the first parameter must be the list of sites to be visited during this excursion
+	//the second parameter should be the list of transports to be used during this excursion
+	Excursion(List<TouristicSite> sites , List<TransportOutil> transports){
+		this.sites=sites;
+		this.transports = transports;	
+	}
 }
