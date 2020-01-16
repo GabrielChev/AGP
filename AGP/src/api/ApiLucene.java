51c433a37fbf9a58b3511624d13031e7529a7f6a
macbookpro <macbookpro@10.77.94.96>
2020-01-16 09:18:10 +0100

    Convert results to objects

--- AGP/src/api/ApiLucene.java
+++ AGP/src/api/ApiLucene.java
@@ -40,6 +40,12 @@ public class ApiLucene {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+	        
+	        
+	        
+	        
+	        
+	        
 		
 	    }
 	
---
+++ AGP/src/buisness/engine/TouristicSiteConverter.java
@@ -0,0 +1,20 @@
+package buisness.engine;
+
+import java.util.ArrayList;
+
+import buisness.objects.TouristicSite;
+
+public class TouristicSiteConverter {
+	
+	public TouristicSiteConverter() {
+	
+	}
+
+	public TouristicSite BuildTouristicSite(ArrayList<String> ResultQuery) {
+		
+		
+		
+		return null ;
+		
+	}
+}
